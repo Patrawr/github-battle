6c54a1462910c76a41479580625ee5f9ac671646
Patrick Wawrzoszek <pwawrzoszek@hotmail.com>
2018-11-05 06:53:13 -0500

    feat: Completed excercise for prop-types.

--- app/index.js
+++ app/index.js
@@ -1,18 +1,36 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
+let PropTypes = require('prop-types');
 require('./index.css'); 
 
-class App extends React.Component {
+class Badge extends React.Component {
     render() {
-        return (
-            <div>
-                Hello Worldy poo!!! Hot reloading!
-            </div>
-        )
+      return (
+        <div>
+          <img 
+            src={this.props.img} 
+            alt='Avatar'
+            style={{width: 100, height: 100}}
+          />
+          <h1>Name: {this.props.name}</h1>
+          <h3>username: {this.props.username}</h3>
+        </div>
+      )
     }
-}
+  }
+  
+  Badge.propTypes = {
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired
+  };
 
-ReactDOM.render(
-    <App />,
+  ReactDOM.render(
+    <Badge 
+      name='Tyler M'
+      username='tylermcginnis'
+      img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'/>,
     document.getElementById('app')
-)
+  );
+
+  
