391e2b723dbf68ddf4865edc0a42a92dd0e3f337
Patrick Wawrzoszek <pwawrzoszek@hotmail.com>
2018-11-08 21:40:19 -0500

    Refactoring language list generator inti it's own stateless function. Passing in props directly, no longer using this within function.

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,30 @@
 let React = require('react');
+let PropTypes = require('prop-types');
 
+function SelectLanguage(props) {
+    var languages = ['All','JavaScript','Ruby','Java','CSS','Python'];
+    
+    return (
+        <ul className='languages'>
+            {languages.map(function (lang) {
+                return (
+                    <li style={lang === props.selectedLanguage ? {color : '#d0021b'} : null}
+                        key={lang}
+                        onClick={props.onSelect.bind(null, lang)}>
+                        {lang}
+                    </li>
+                )
+            })
+
+            }
+        </ul>
+    )
+}
+
+SelectLanguage.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired
+}
 class Popular extends React.Component {
     constructor (props) {
         super(props);
@@ -19,21 +44,13 @@ class Popular extends React.Component {
     }
 
     render(){
-        var languages = ['All','JavaScript','Ruby','Java','CSS','Python'];
-        return (
-            <ul className='languages'>
-                {languages.map(function (lang) {
-                    return (
-                        <li style={lang === this.state.selectedLanguage ? {color : '#d0021b'} : null}
-                            key={lang}
-                            onClick={this.updateLanguage.bind(null, lang)}>
-                            {lang}
-                        </li>
-                    )
-                }, this)
-
-                }
-            </ul>
+        return(
+            <div>
+                <SelectLanguage 
+                    selectedLanguage={this.state.selectedLanguage}
+                    onSelect={this.updateLanguage}
+                    />
+            </div>
         )
     }
 };
