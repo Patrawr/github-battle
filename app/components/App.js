5dd13b768c2c8bfb3976df15748d6d7e05a2083f
Patrick Wawrzoszek <pwawrzoszek@hotmail.com>
2018-11-11 14:59:30 -0500

    feat: Added react router routes and Home and Battle components (as well as their routing)

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,31 @@
 let React = require('react');
 let Popular = require('./Popular');
+let Home = require('./Home');
+let Battle = require('./Battle');
+let Nav = require('./Nav');
+
+let ReactRouter = require('react-router-dom');
+let Router = ReactRouter.BrowserRouter;
+let Route = ReactRouter.Route;
+let Switch = ReactRouter.Switch;
 
 class App extends React.Component {
     render() {
         return (
-            <div className='container'>
-                <Popular />
-            </div>
+            <Router>
+                <div className='container'>
+                    <Nav />
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/battle' component={Battle} />
+                        <Route path='/popular' component={Popular} />
+                        <Route render={function () {
+                            return <p>Not Found</p>
+                        }} />
+                    </Switch>
+                </div>
+            </Router>
+            
         )
     }
 }
---
+++ app/components/Battle.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+class Battle extends React.Component {
+    render () {
+        return (
+            <div>
+                FIGHT!
+            </div>
+        )
+    }
+};
+
+module.exports = Battle;
+
