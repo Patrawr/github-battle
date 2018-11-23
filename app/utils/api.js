5e97d25eea2592f94c655dc5290322e1ac179cb3
Patrick Wawrzoszek <pwawrzoszek@hotmail.com>
2018-11-22 20:47:03 -0500

    feat: Adding player preview component to results page, fleshing out details during battle.

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,17 +2,17 @@ let axios = require('axios');
 
 let id = "YOUR_CLIENT_ID";
 let sec = "YOUR_SECRET_ID";
-var params = "?client_id=" + id + "&client_secret" + sec; 
+var params = "?client_id=" + id + "&client_secret=" + sec; 
 
 function getProfile (username) {
-    return axios.get('https://api.github.com/users' + username)
+    return axios.get('https://api.github.com/users/' + username)
         .then(function (user) {
             return user.data;
         });
 }
 
 function getRepos (username) {
-    return axios.get('https://api.github.com/users/' + username + '/repos' + '&per_page=100')
+    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
 }
 
 function getStarCount (repos) {
